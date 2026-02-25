ca05540df245dabafd3571bf456bb9be94f49f23
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-02-25 12:03:13 +0100

    Add POST route for adding books

--- backend/src/routes/books.ts
+++ backend/src/routes/books.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { type Request, type Response } from 'express';
-import type { Book } from '../lib/utils.js';
+import { type APIResponse, type Book, formatISBN, getBook } from '../lib/utils.js';
 import {DataManager} from "../lib/dataManager.js";
 
 enum SortBy {
@@ -63,6 +63,28 @@ router.route('/:isbn')
         if (book == undefined) return res.status(404).json({error: 'Book not found'});
 
         res.status(200).send(book);
+    })
+    .post(async (req: Request, res: Response) => {
+        let ISBNdata = await DataManager.getISBNs();
+        const rawISBN = req.params.isbn;
+        if (rawISBN == null) return res.status(400).json({error: 'Invalid ISBN'});
+        if (typeof rawISBN !== 'string') return res.status(400).json({error: 'Invalid ISBN'});
+
+        const response: APIResponse = await getBook(rawISBN);
+        if (response.status !== 200) return res.status(response.status).json({error: response.error});
+        const entry: Book = response.data;
+
+        const cleanIsbn: string = rawISBN.replace(/-/g, '');
+
+        if (!ISBNdata.includes(cleanIsbn)) {
+            const formattedIsbn: string | undefined = formatISBN(cleanIsbn);
+            const isbn: string = formattedIsbn !== undefined ? formattedIsbn : cleanIsbn;
+            ISBNdata = [...ISBNdata, isbn];
+            await DataManager.saveISBN(ISBNdata);
+            res.status(201).json({message: `Book '${entry.title}' by '${entry.author}' added to sync list`});
+        } else {
+            res.status(200).json({message: 'Book already in list'});
+        }
     })
 
 export default router;
