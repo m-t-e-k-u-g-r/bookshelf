e89b3626d5445af606e1c3d426fe0a5d5af84668
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-03-02 21:46:16 +0100

    Add functions to handle CRUD operations for books

--- backend/src/lib/dbDataManager.ts
+++ backend/src/lib/dbDataManager.ts
@@ -8,3 +8,46 @@ const pool: Pool = mariadb.createPool({
     database: process.env.MARIADB_DATABASE || '',
     connectionLimit: 10,
 });
+
+export class DbDataManager {
+    static async getBooks() {
+        try {
+            return await pool.query(`SELECT * FROM books`);
+        } catch (e) {
+            throw e;
+        }
+    }
+    static async getBookByISBN(isbn: string) {
+        try {
+            return await pool.query(`
+                SELECT * FROM books 
+                WHERE isbn = ?
+                `, [isbn]
+            );
+        } catch (e) {
+            throw e;
+        }
+    }
+    static async addBook(isbn: string, isbn_h: string, title: string, author: string, publish_year: number, img_url: string) {
+        try {
+            return await pool.query(`
+                INSERT INTO books (isbn, isbn_h, title, author, publish_year, img_url) 
+                VALUES (?, ?, ?, ?, ?, ?)
+                `, [isbn, isbn_h, title, author, publish_year, img_url]
+            );
+        } catch (e) {
+            throw e;
+        }
+    }
+    static async deleteBook(isbn: string) {
+        try {
+            return await pool.query(`
+                DELETE FROM books 
+                WHERE isbn = ?
+                `, [isbn]
+            );
+        } catch (e) {
+            throw e;
+        }
+    }
+}
