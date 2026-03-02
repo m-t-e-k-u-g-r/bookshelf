a0fb55a1043ffa8b1fb5e28ee8efdd8e75ee85fc
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-03-02 19:23:04 +0100

    Implement functionality to add batch of books

---
+++ frontend/src/components/CombinedInput.tsx
@@ -0,0 +1,48 @@
+import CustomButton from "./CustomButton";
+import {addBatch} from "../dataHandler";
+import {type ChangeEvent, useState} from "react";
+
+export default function CombinedInput({ close }: { close: () => void }) {
+    const [content, setContent] = useState<string[]>([]);
+
+    async function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+        if (e.target.files) {
+            const file: File | undefined = e.target.files[0];
+            if (!file) return;
+
+            const content: string = await file.text();
+            const values: string[] = content
+                .split(/[\r\n,;]+/)
+                .map(v => v.trim())
+                .filter(v => v !== "");
+
+            setContent(values);
+        }
+    }
+
+    return (
+        <div className={`combined_input`}>
+            <textarea
+                wrap={'soft'}
+                value={content.join("\n")}
+                onChange={(e) => {
+                    const lines = e.target.value.split(/\r?\n/);
+                    setContent(lines);
+                }}
+            />
+            <p>Please enter one ISBN per line</p>
+            <input
+                type={'file'}
+                accept={'.csv, .txt'}
+                onChange={handleFileChange}
+            />
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <CustomButton onClick={() => {
+                    addBatch(content)
+                    close();
+                }} children={'Add batch'}/>
+                <CustomButton onClick={close} children={'Close'}/>
+            </div>
+        </div>
+    );
+}
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,15 +1,28 @@
 import CustomButton from './CustomButton.js';
 import { reload, addBook } from "../dataHandler";
+import CombinedInput from "./CombinedInput";
+import {useRef} from "react";
 
 
 export default function Nav() {
+    const dialogRef = useRef<HTMLDialogElement>(null);
+    function openDialog() {
+        dialogRef.current?.showModal();
+    }
+    function closeDialog() {
+        dialogRef.current?.close();
+    }
 
     return (
         <>
             <nav>
                 <CustomButton onClick={reload} children={'Reload'}/>
+                <CustomButton onClick={openDialog} children={'Add Batch'}/>
                 <CustomButton onClick={addBook} children={'Add Book'}/>
             </nav>
+            <dialog ref={dialogRef}>
+                <CombinedInput close={closeDialog}/>
+            </dialog>
         </>
     );
 }
