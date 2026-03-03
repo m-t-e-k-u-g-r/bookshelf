fcceb9f057f8fdb5937056f9fa6b1b2529a0984f
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-03-03 17:34:28 +0100

    Add shelves router for database operations

---
+++ backend/src/routes/db_shelves.ts
@@ -0,0 +1,89 @@
+import express from 'express';
+import { type Router, type Request, type Response } from "express";
+import {DbDataManager as db, type BookInShelf, type SidebarData} from '../lib/dbDataManager.js';
+
+const router: Router = express.Router();
+
+router.route('/')
+    .get(async (req: Request, res: Response) => {
+        // #swagger.tags = ['DB Shelves']
+        try {
+            const shelves: BookInShelf[] = await db.getShelvesWithBooks();
+            return res.status(200).send(shelves);
+        } catch (e) {
+            return res.status(500).json({error: 'Failed to get shelves with books'});
+        }
+    })
+    .post(async (req: Request, res: Response) => {
+        // #swagger.tags = ['DB Shelves']
+        const isbn: string = req.body.isbn;
+        const selectedShelvesId: string[] = req.body.shelves;
+        if (isbn == undefined || selectedShelvesId == undefined) return res.status(400).json({error: 'Invalid request body'});
+
+        try {
+            await db.editShelvesOfBook(isbn, selectedShelvesId);
+            return res.sendStatus(201);
+        } catch (e) {
+            return res.status(500).json({error: 'Failed to edit shelves of book'});
+        }
+    })
+    .put(async (req: Request, res: Response) => {
+        // #swagger.tags = ['DB Shelves']
+        const oldShelfName: string = req.body.oldShelfName;
+        const newShelfName: string = req.body.newShelfName;
+        if (oldShelfName == undefined || newShelfName == undefined) return res.status(400).json({error: 'Invalid request body'});
+
+        try {
+            await db.updateShelfName(oldShelfName, newShelfName);
+            return res.sendStatus(200);
+        } catch (e) {
+            return res.status(500).json({error: 'Failed to update shelf name'});
+        }
+    });
+
+router.route('/sidebar').get(async (req: Request, res: Response) => {
+    // #swagger.tags = ['DB Shelves']
+    try {
+        const response: SidebarData[] = await db.getSidebarData();
+        return res.status(200).send(response);
+    } catch (e) {
+        return res.status(500).json({error: 'Failed to get sidebar data'});
+    }
+});
+
+router.route('/:shelfName')
+    .get(async (req: Request, res: Response) => {
+        // #swagger.tags = ['DB Shelves']
+        const shelfName = req.params.shelfName;
+        if (typeof shelfName !== 'string') return res.status(400).json({error: 'Invalid shelf name'});
+        try {
+            const response: BookInShelf[] = await db.getBooksByShelf(shelfName);
+            return res.status(200).send(response);
+        } catch (e) {
+            return res.status(500).json({error: `Failed to get books in shelf ${shelfName}`});
+        }
+    })
+    .post(async (req: Request, res: Response) => {
+        // #swagger.tags = ['DB Shelves']
+        const shelfName = req.params.shelfName;
+        if (typeof shelfName !== 'string') return res.status(400).json({error: 'Invalid shelf name'});
+        try {
+            await db.addShelf(shelfName);
+            return res.status(201).json({ message: `Shelf '${shelfName}' created.` });
+        } catch (e) {
+            return res.status(500).json({error: 'Failed to create shelf'});
+        }
+    })
+    .delete(async (req: Request, res: Response) => {
+        // #swagger.tags = ['DB Shelves']
+        const shelfName = req.params.shelfName;
+        if (typeof shelfName !== 'string') return res.status(400).json({error: 'Invalid shelf name'});
+        try {
+            await db.deleteShelf(shelfName);
+            return res.sendStatus(204);
+        } catch (e) {
+            return res.status(500).json({error: 'Failed to delete shelf'});
+        }
+    });
+
+export default router;
