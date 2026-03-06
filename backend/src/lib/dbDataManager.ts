0ef062a9f832a45f1eabc3b8036790f315f8b5d7
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-03-06 22:09:41 +0100

    Refactor dbDataManager.ts to make data retrieval and handling more fault-tolerant

--- backend/src/lib/dbDataManager.ts
+++ backend/src/lib/dbDataManager.ts
@@ -2,17 +2,35 @@ import * as mariadb from 'mariadb';
 import {type Pool, type PoolConnection} from 'mariadb';
 
 let pool: Pool | null = null;
-function getPool(): Pool {
-    if (!pool) {
-        pool = mariadb.createPool({
-            host: 'localhost',
-            user: process.env.MARIADB_USER || '',
-            password: process.env.MARIADB_PASSWORD || '',
-            database: process.env.MARIADB_DATABASE || '',
-            connectionLimit: 10,
-            bigIntAsNumber: true,
-        });
+
+async function createPool(): Promise<Pool> {
+    if (pool) return pool;
+
+    pool = mariadb.createPool({
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: 3306,
+        user: process.env.MARIADB_USER || '',
+        password: process.env.MARIADB_PASSWORD || '',
+        database: process.env.MARIADB_DATABASE || '',
+        connectionLimit: 10,
+        bigIntAsNumber: true,
+    });
+
+    while (true) {
+        try {
+            const conn = await pool.getConnection();
+            conn.release();
+            break;
+        } catch (err) {
+            await new Promise(r => setTimeout(r, 3000));
+        }
     }
+
+    return pool;
+}
+
+export async function getPool(): Promise<Pool> {
+    const pool: Pool = await createPool();
     return pool;
 }
 
@@ -39,14 +57,16 @@ export interface SidebarData {
 export class DbDataManager {
     static async getBooks(): Promise<Book[]> {
         try {
-            return await getPool().query(`SELECT * FROM books`);
+            const pool: Pool = await getPool();
+            return await pool.query(`SELECT * FROM books`);
         } catch (e) {
             throw e;
         }
     }
     static async getBookByISBN(isbn: string): Promise<Book | undefined> {
         try {
-            const result = await getPool().query(`
+            const pool: Pool = await getPool();
+            const result = pool.query(`
                 SELECT * FROM books 
                 WHERE isbn = ?
                 `, [isbn]
@@ -61,8 +81,9 @@ export class DbDataManager {
     }
     static async addBook(book: Book) {
         try {
-            return await getPool().query(`
-                INSERT INTO books (isbn, isbn_h, title, author, publish_year, img_url) 
+            const pool: Pool = await getPool();
+            return await pool.query(`
+                INSERT INTO books (isbn, isbn_h, title, author, publish_year, img_url)
                 VALUES (?, ?, ?, ?, ?, ?)
                 `, [book.isbn, book.isbn_h, book.title, book.author, book.publish_date, book.imgUrl]
             );
@@ -74,7 +95,8 @@ export class DbDataManager {
         }
     }
     static async addBatch(books: Book[]) {
-        const connection: PoolConnection = await getPool().getConnection();
+        const pool: Pool = await getPool();
+        const connection: PoolConnection = await pool.getConnection();
         try {
             await connection.beginTransaction();
 
@@ -97,8 +119,9 @@ export class DbDataManager {
         }
     }
     static async deleteBook(isbn: string) {
+        const pool: Pool = await getPool();
         try {
-            return await getPool().query(`
+            return pool.query(`
                 DELETE FROM books 
                 WHERE isbn = ?
                 `, [isbn]
@@ -108,39 +131,44 @@ export class DbDataManager {
         }
     }
     static async getISBNs(): Promise<ISBNList> {
+        const pool: Pool = await getPool();
         try {
-            const result: Book[] = await getPool().query(`SELECT isbn FROM books`);
+            const result: Book[] = await pool.query(`SELECT isbn FROM books`);
             return result.map((row: Book) => row.isbn);
         } catch (e) {
             throw e;
         }
     }
     static async getFormatedISBNs(): Promise<ISBNList> {
+        const pool: Pool = await getPool();
         try {
-            const result = await getPool().query(`SELECT isbn_h FROM books`);
+            const result = await pool.query(`SELECT isbn_h FROM books`);
             return result.map((row: Book) => row.isbn_h);
         } catch (e) {
             throw e;
         }
     }
     static async getShelfNames(): Promise<string[]> {
+        const pool: Pool = await getPool();
         try {
-            const result = await getPool().query(`SELECT name FROM shelves`);
+            const result = await pool.query(`SELECT name FROM shelves`);
             return result.map((row: { name: string }) => row.name);
         } catch (e) {
             throw e;
         }
     }
     static async getShelvesWithBooks(): Promise<BookInShelf[]> {
+        const pool: Pool = await getPool();
         try {
-            return await getPool().query(`SELECT * FROM books_in_shelves`);
+            return pool.query(`SELECT * FROM books_in_shelves`);
         } catch (e) {
             throw e;
         }
     }
     static async getShelvesOfBook(isbn: string): Promise<string[] | undefined> {
+        const pool: Pool = await getPool();
         try {
-            const result = await getPool().query(`
+            const result = pool.query(`
                 SELECT iis.name AS shelf
                 FROM isbns_in_shelves iis
                 WHERE iis.isbn = ?
@@ -155,8 +183,9 @@ export class DbDataManager {
         }
     }
     static async getBooksByShelf(shelfName: string): Promise<BookInShelf[]> {
+        const pool: Pool = await getPool();
         try {
-            return await getPool().query(`
+            return pool.query(`
             SELECT * FROM books_in_shelves
             WHERE shelf = ?
             `, [shelfName])
@@ -165,15 +194,17 @@ export class DbDataManager {
         }
     }
     static async getSidebarData(): Promise<SidebarData[]> {
+        const pool: Pool = await getPool();
         try {
-            return await getPool().query(`SELECT * FROM sidebar_data`);
+            return pool.query(`SELECT * FROM sidebar_data`);
         } catch (e) {
             throw e;
         }
     }
     static async addShelf(shelfName: string) {
+        const pool: Pool = await getPool();
         try {
-            return await getPool().query(`
+            return pool.query(`
                 INSERT INTO shelves (name) 
                 VALUES (?)
                 `, [shelfName]
@@ -183,8 +214,9 @@ export class DbDataManager {
         }
     }
     static async updateShelfName(oldName: string, newName: string) {
+        const pool: Pool = await getPool();
         try {
-            return await getPool().query(`
+            return pool.query(`
                 UPDATE shelves 
                 SET name = ?
                 WHERE name = ?
@@ -195,8 +227,9 @@ export class DbDataManager {
         }
     }
     static async deleteShelf(shelfName: string) {
+        const pool: Pool = await getPool();
         try {
-            return await getPool().query(`
+            return pool.query(`
                 DELETE FROM shelves 
                 WHERE name = ?
                 `, [shelfName]
@@ -206,7 +239,8 @@ export class DbDataManager {
         }
     }
     static async editShelvesOfBook(isbn: string, shelves: string[]) {
-        const connection: PoolConnection = await getPool().getConnection();
+        const pool: Pool = await getPool();
+        const connection: PoolConnection = await pool.getConnection();
         try {
             await connection.beginTransaction();
 
