46042e6736bb8e5124892cbee744eda56998d98a
m-t-e-k-u-g-r <matteo.krueger.ch@gmail.com>
2026-06-19 23:15:33 +0200

    feat: update routing to redirect to home instead of displaying not found component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import {NotFoundComponent} from './components/not-found/not-found.component';
 import {LoginComponent} from './components/login/login.component';
 import {authGuard} from './guards/auth.guard';
 import {HomeComponent} from './components/home/home.component';
@@ -8,5 +7,5 @@ export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [authGuard] },
   { path: 's/:shelfId', component: HomeComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: '**', redirectTo: '' }
 ];
